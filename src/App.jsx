cfec3eae1f6efc1654de810ed243baeb0acd34b6
BogushF <boga1602@gmail.com>
2024-07-28 18:13:02 +0200

    react-hw-04

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,83 @@
-import userData from "./userData.json";
-import Profile from "./components/Profile/Profile.jsx";
-import friends from "./friends.json";
-import FriendList from "./components/FriendList/FriendList.jsx";
-import transactions from "./transactions.json";
-import TransactionHistory from "./components/TransactionHistory/TransactionHistory.jsx";
+import { useEffect, useState } from "react";
+import fetchImages from "./API";
+import ImageGallery from "./components/ImageGallery/ImageGallery";
+import SearchBar from "./components/SearchBar/SearchBar";
+import Loader from "./components/Loader/Loader";
+import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
+import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn";
+import ImageModal from "./components/ImageModal/ImageModal";
 
-const App = () => {
+export const App = () => {
+  const [query, setQuery] = useState("");
+  const [cards, setCards] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [totalPages, setTotalPages] = useState(1);
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [modalImage, setModalImage] = useState({ url: "", alt: "" });
+
+  useEffect(() => {
+    if (query === "") {
+      return;
+    }
+    async function getData() {
+      try {
+        setIsLoading(true);
+        setIsError(false);
+        const { results, totalPages } = await fetchImages(query, page);
+        setCards((prevCards) => {
+          return [...prevCards, ...results];
+        });
+        setTotalPages(totalPages);
+      } catch (error) {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    getData();
+  }, [page, query]);
+
+  const handleSearch = async (query) => {
+    setQuery(query);
+    setPage(1);
+    setCards([]);
+  };
+
+  const handleLoadMore = () => {
+    setPage(page + 1);
+  };
+
+  const handleImgClick = (url, alt) => {
+    setModalImage({ url, alt });
+    setIsOpen(true);
+  };
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
-    <div>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
-
-      <FriendList friends={friends} />
-
-      <TransactionHistory items={transactions} />
-    </div>
+    <>
+      <SearchBar onSearch={handleSearch} />
+      {isError && <ErrorMessage />}
+      {cards.length > 0 && !isError && (
+        <ImageGallery cards={cards} onImgClick={handleImgClick} />
+      )}
+      {isLoading && !isError && <Loader />}
+      {!isLoading && cards.length > 0 && !isError && page < totalPages && (
+        <LoadMoreBtn onClick={handleLoadMore} />
+      )}
+      {modalIsOpen && (
+        <ImageModal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          imageUrl={modalImage.url}
+          imageAlt={modalImage.alt}
+        />
+      )}
+    </>
   );
 };
 
